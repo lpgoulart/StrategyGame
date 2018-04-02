e837707156bb6c8f7a910b91541d35dbf95e6c80
Leonardo Peres de Melo Goulart <lpgoulart@A305-25L.imd.local>
2018-04-02 20:03:33 -0300

    Changed javadoc syntax, contructor, localizacao and more

--- Castelo.java
+++ Castelo.java
@@ -1,91 +1,104 @@
 /**
 *	@author Leonardo Goulart
+*	@author Arthur Cohen
 *	@version 1.0
 */
 
 public class Castelo {
 	
 	protected double defesas;
-	protected double localizacao;
+	protected int[] localizacao;
 	protected double pontosVida;
 	protected String nome;
 
 	/**
 	 *	Construtor
+	 *  @param defesas Os pontos de defesa que o castelo possui
+	 *  @param localizacao Um vetor unidimensional de tamanho 2 que representa a localizacao do castelo no tabuleiro como um par ordenado
+	 *  @param pontosVida Os pontos de vida que o castelo possui
+	 *  @param nome Representacao textual do castelo
 	 */
-	Castelo () {
-
-	 }
-
-	/**
-	 *	Mostrar situacao
-	 */
-	public void mostrarSituacao() {
-
-	 }
-
-	/**
-	 *	Realiza um ataque
-	 *	@param dano Recebe um valor de ataque
-	 */
-	public void ataque ( double dano ) {
-		this.pontosVida -= dano;
-	 }
-
-	/**
- 	 *	@param defesas Da um novo valor para a variavel *defesas
-	 */
-	public void setDefesas ( double defesas ) {
+	Castelo (double defesas, int[] localizacao, double pontosVida, String nome) {
 		this.defesas = defesas;
-	 }
-
-	/**
-	 *	@return Retorna a variavel *defesas
-	 */	
-	public double getDefesas () {
-		return this.defesas;
-	 }
-
-	/**
-	 *	@param localiacao Da um novo valor para a variavel *localizacao
-	 */
-	public void setLocalizacao ( double localizacao ) {
 		this.localizacao = localizacao;
-	 }
+		this.pontosVida = pontosVida;
+		this.nome = nome;
+	}
+
+	/**
+	 *	Retorna todas informacoes importantes sobre o castelo
+	 *  @return Informacoes sobre o castelo
+	 */
+	public String mostrarSituacao(){
+		return "";
+	}
+
+	/**
+	 *	Realiza o calculo de um ataque sofrido
+	 *	@param dano O valor que representa o ataque sofrido
+	 */
+	public void ataque(double dano){
+		if (this.defesas > 0){
+			this.defesas -= dano;
+		}else{
+			this.pontosVida -= dano;
+		}
+	}
+
+	/**
+ 	 *	@param defesas Da um novo valor para a variavel defesas
+	 */
+	public void setDefesas(double defesas){
+		this.defesas = defesas;
+	}
+
+	/**
+	 *	@return Retorna a variavel defesas
+	 */	
+	public double getDefesas() {
+		return this.defesas;
+	}
+
+	/**
+	 *	@param localiacao Da um novo valor para a variavel localizacao
+	 */
+	public void setLocalizacao(int[] localizacao){
+		this.localizacao = localizacao;
+	}
 
 	/**
 	 *	@return Retorna o valor da variavel Localizacao	
 	 */	
-	public double getLocalizacao () {
+	public int[] getLocalizacao(){
 		return this.localizacao;
-	 }
+	}
 
 	/**
-	 *	@param pontosVida Da um novo valor para a variavel *pontosVida
+	 *	@param pontosVida Da um novo valor para a variavel pontosVida
 	 */
-	public void setPontosVida ( double pontosVida ) {
+	public void setPontosVida(double pontosVida){
 		this.pontosVida = pontosVida;
-	 }
+	}
 
 	/**
-	 *	@return Retorna o valor da variavel *pontosVida
+	 *	@return Retorna o valor da variavel pontosVida
 	 */	
-	public double getPontosVida () {
+	public double getPontosVida(){
 		return this.pontosVida;
-	 }
+	}
 
 	/**
-	 *	@param nome Da um novo valor para a variavel *nome
+	 *	@param nome Da um novo valor para a variavel nome
 	 */
-	public void setNome ( String nome ) {
+	public void setNome(String nome){
 		this.nome = nome;
-   	 }
+   	}
 
 	/**
 	 *	@return Retorna a variavel *nome
 	 */	
-	public String getNome () {
+	public String getNome(){
 		return this.nome;
-	 }
+	}
 
 }
