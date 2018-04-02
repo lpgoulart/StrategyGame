cb363e675f7f8ea7bcfd9c2e9e7dc4f011a02eaa
Leonardo Peres de Melo Goulart <lpgoulart@A305-25L.imd.local>
2018-04-02 20:04:31 -0300

    Changed the unit type (now is a List), the ataque method, javadoc and more

--- Europeu.java
+++ Europeu.java
@@ -1,45 +1,58 @@
 /**
 *	@author Leonardo Goulart
+*	@author Arthur Cohen
 *	@version 1.0
 */
+import java.util.List;
+import java.util.ArrayList;
 
 public class Europeu extends Castelo {
-	protected int numPaladinos;
+	private List<Paladino> paladinos;
 
 	/**
 	 *	Construtor
+	 *  @param defesas Os pontos de defesa que o castelo possui
+	 *  @param localizacao Um vetor unidimensional de tamanho 2 que representa a localizacao do castelo no tabuleiro como um par ordenado
+	 *  @param pontosVida Os pontos de vida que o castelo possui
+	 *  @param nome Representacao textual do castelo
 	 */
-	Europeu () {
-		super ();
-	 }
+	Europeu(double defesas, int[] localizacao, double pontosVida, String nome){
+		super(defesas, localizacao, pontosVida, nome);
+		this.paladinos = new ArrayList<Paladino>();
+	}
 
 	/**
 	 *	Mostrar situacao
+	 *  @return Informacoes sobre o castelo
 	 */
-	public void mostrarSituacao() {
-		
-	 }
+	public String mostrarSituacao() {
+		return "";
+	}
 
 	/**
-	 *	Realiza um ataque
-	 *	@param Recebe um valor de ataque
+	 *	Realiza o calculo de um ataque sofrido
+	 *	@param dano O valor que representa o ataque sofrido
 	 */
-	public void ataque ( double dano ) {
-		this.pontosVida -= dano;
-	 }
+	public void ataque(double dano){
+		if (this.paladinos.size() > 0){
+			// Matar de 2 a 5 guerreiros
+			
+		}else{
+			super.ataque(dano);
+		}
+	}
 
 	/**
-	 *	@param Da um novo valor para a variavel *numPaladinos
+	 *	@param paladinos Da um novo valor para a variavel paladinos
 	 */
-	public void setNumPaladinos ( int numPaladinos ) {
-		this.numPaladinos = numPaladinos;
-	 }
+	public void setPaladinos(List<Paladino> paladinos){
+		this.paladinos = paladinos;
+	}
 
 	/**
-	 *	@return Retorna o valor de *numPaladinos
+	 *	@return Retorna o valor de paladinos
 	 */	
-	public int getNumPaladinos () {
-		return this.numPaladinos;
-	 }
-
+	public List<Paladino> getPaladinos(){
+		return this.paladinos;
+	}
 }
--- Japones.java
+++ Japones.java
@@ -1,44 +1,58 @@
 /**
 *	@author Leonardo Goulart
+*	@author Arthur Cohen
 *	@version 1.0
 */
+import java.util.List;
+import java.util.ArrayList;
 
 public class Japones extends Castelo {
-	protected int numSamurais;
+	private List<Samurai> samurais;
 
 	/**
 	 *	Construtor
+	 *  @param defesas Os pontos de defesa que o castelo possui
+	 *  @param localizacao Um vetor unidimensional de tamanho 2 que representa a localizacao do castelo no tabuleiro como um par ordenado
+	 *  @param pontosVida Os pontos de vida que o castelo possui
+	 *  @param nome Representacao textual do castelo
 	 */
-	Japones () {
-		super ();
-	 }
+	Japones(double defesas, int[] localizacao, double pontosVida, String nome){
+		super(defesas, localizacao, pontosVida, nome);
+		this.samurais = new ArrayList<Samurai>();
+	}
 
 	/**
 	 *	Mostrar situacao
+	 *  @return Informacoes sobre o castelo
 	 */
-	public void mostrarSituacao() {
-		
-	 }
+	public String mostrarSituacao(){
+		return "";
+	}
 
 	/**
-	 *	Realiza um ataque
-	 *	@param Recebe um valor de ataque
+	 *	Realiza o calculo de um ataque sofrido
+	 *	@param dano O valor que representa o ataque sofrido
 	 */
-	public void ataque ( double dano ) {
- 		this.pontosVida -= dano;
-	 }
+	public void ataque(double dano){
+ 		if (this.samurais.size() > 0){
+			// Matar de 2 a 5 guerreiros
+			
+		}else{
+			super.ataque(dano);
+		}
+	}
 
 	/**
 	 *	@param Da um novo valor para a variavel *numSamurais
 	 */
-	public void setNumSamurais ( int numSamurais ) {
-		this.numSamurais = numSamurais;
-	 }
+	public void setSamurais(List<Samurai> samurais){
+		this.samurais = samurais;
+	}
 
 	/**
 	 *	@return Retorna o valor de *numSamurais
 	 */	
-	public int getNumSamurais () {
-		return this.numSamurais;
-	 }
+	public List<Samurai> getSamurais(){
+		return this.samurais;
+	}
 }
